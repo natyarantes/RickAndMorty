cadbbcdfaa96578e3b7879cdb02ee561a2e1e95f
Nat Arantes <anatyarantes@gmail.com>
2020-07-31 16:34:56 -0300

    Initial Commit

---
+++ RickAndMorty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Natália Arantes on 31/07/20.
+//  Copyright © 2020 Natália Arantes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
