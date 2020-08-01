82acd94fefd4e126cb16308e1859660fb346c005
Nat Arantes <anatyarantes@gmail.com>
2020-08-01 20:00:42 -0300

    First screen ready

---
+++ RickAndMorty/Extensions/UIColor+Extension.swift
@@ -0,0 +1,52 @@
+//
+//  UIColor+Extension.swift
+//  testes
+//
+//  Created by Natália Arantes on 31/07/20.
+//  Copyright © 2020 Natália Arantes. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    //Colors | Accent
+    @nonobjc class var indigo: UIColor {
+        return UIColor(named: "Indigo")!
+    }
+    //Colors | Graybase
+    @nonobjc class var gray1: UIColor {
+        return UIColor(named: "Gray 1")!
+    }
+    @nonobjc class var gray2: UIColor {
+        return UIColor(named: "Gray 2")!
+    }
+    @nonobjc class var gray3: UIColor {
+        return UIColor(named: "Gray 3")!
+    }
+    @nonobjc class var gray4: UIColor {
+        return UIColor(named: "Gray 4")!
+    }
+    @nonobjc class var gray5: UIColor {
+        return UIColor(named: "Gray 5")!
+    }
+    @nonobjc class var gray6: UIColor {
+        return UIColor(named: "Gray 6")!
+    }
+    //Colors | Basic
+    @nonobjc class var adding: UIColor {
+        return UIColor(named: "Adding")!
+    }
+    @nonobjc class var basicblack: UIColor {
+        return UIColor(named: "Basic Black")!
+    }
+    @nonobjc class var destructive: UIColor {
+        return UIColor(named: "Destructive")!
+    }
+    @nonobjc class var grey: UIColor {
+        return UIColor(named: "Grey")!
+    }
+    @nonobjc class var white: UIColor {
+        return UIColor(named: "White")!
+    }
+}
---
+++ RickAndMorty/ViewController/RickAndMortyTabBarContoller.swift
@@ -0,0 +1,9 @@
+//
+//  RickAndMortyTabBarContoller.swift
+//  RickAndMorty
+//
+//  Created by Natália Arantes on 31/07/20.
+//  Copyright © 2020 Natália Arantes. All rights reserved.
+//
+
+import Foundation
