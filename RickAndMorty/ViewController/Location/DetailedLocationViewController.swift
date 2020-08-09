dea724a2af5a7082c7f26350b2dd51c7f1a45105
Nat Arantes <anatyarantes@gmail.com>
2020-08-09 16:07:34 -0300

    changes on structure, launchscreen and main storyboard

---
+++ RickAndMorty/ViewController/Location/DetailedLocationViewController.swift
@@ -0,0 +1,13 @@
+//
+//  DetailedLocationViewController.swift
+//  RickAndMorty
+//
+//  Created by Natália Arantes on 09/08/20.
+//  Copyright © 2020 Natália Arantes. All rights reserved.
+//
+
+import UIKit
+
+class DetailedLocationViewController: UIViewController {
+    
+}
